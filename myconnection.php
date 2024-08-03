df764ddc38f63af8499317d0d7c9eb770c82bc13
Myk <manuhwamcihealn@gmail.com>
2024-08-03 13:48:39 +0200

    finished linking my database

--- myconnection.php
+++ myconnection.php
@@ -1,20 +1,23 @@
-<? 
-$lastName = $_POST['lastName'];
-$firstName = $_POST['firstName'];
-$gender = $_POST['gender'];
-$email = $_POST['email'];
-$Password = $_POST['password'];
-$number = $_POST['number'];
+<?php
+	$lastName = $_POST['lastName'];
+	$firstName = $_POST['firstName'];
+	$gender = $_POST['gender'];
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	$number = $_POST['number'];
 
-
-//creating my database
-
-$conn = new mysqli('localhost'root','','test');
-if($conn->connect_error){
-die('connection failed : '.conn->connect_error);
-
-}else{
-$stmt = $conn->prepare("insert into registration(lastName, firstName, gender, email, password, number) values(?, ?, ?, ?, ?)")
-
-}
+	// Database connection
+	$conn = new mysqli('localhost','root','','test');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into registration(firstName, lastName, gender, email, password, number) values(?, ?, ?, ?, ?, ?)");
+		$stmt->bind_param("sssssi", $lastName, $firstName, $gender, $email, $password, $number);
+	    $stmt->execute();
+		echo $execval;
+		echo "Registration successfully...";
+		$stmt->close();
+		$conn->close();
+	}
 ?>
